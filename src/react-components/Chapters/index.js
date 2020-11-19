3cd173692cf6c67d83ba501ee5d50b051d324ecb
Jedid Ahn <jedid.ahn@mail.utoronto.ca>
2020-11-18 23:10:58 -0500

    Adding logos and started draft of chapters page.

--- src/react-components/Chapters/index.js
+++ src/react-components/Chapters/index.js
@@ -1,78 +1,50 @@
 import React, { Component } from "react";
 import { withRouter, BrowserRouter } from "react-router-dom";
-import { Row, Col, Image, Form, Button } from "react-bootstrap";
-import { FaExclamationCircle } from "react-icons/fa";
+import { Row, Col, Image, Button } from "react-bootstrap";
 
 import "./styles.css";
-import logo from "./static/logo.png";
+import gsggb_logo from "./static/gsggb-logo.png";
+import uoft_logo from "./static/uoft-logo.png";
+import mcmaster_logo from "./static/mcmaster-logo.png";
+import queens_logo from "./static/queens-logo.png";
 
 class Chapters extends Component {
     render() {
         return (
-            <div className="login-page">
-                <BrowserRouter forceRefresh={true}>
-                    <div className="login-form">
-                        <Row>
-                            <Col md={6}>
-                                <Image id="logo" alt="logo" src={logo} />
-                            </Col>
-
-                            <Col md={6}>
-                                <h1 className="login-title">Login</h1>
-                                <div
-                                    className="alert alert-danger text-center"
-                                    id="login-error"
-                                >
-                                    <FaExclamationCircle />
-                                    &nbsp; Invalid username or password. Please
-                                    try again.
-                                </div>
-                                <Form>
-                                    <Form.Group>
-                                        <Form.Label id="login-label">
-                                            Username
-                                        </Form.Label>
-                                        <Form.Control
-                                            id="user-name"
-                                            type="text"
-                                            placeholder="Type your username"
-                                            required
-                                        />
-                                    </Form.Group>
-
-                                    <Form.Group>
-                                        <Form.Label id="login-label">
-                                            Password
-                                        </Form.Label>
-                                        <Form.Control
-                                            id="user-pass"
-                                            type="password"
-                                            placeholder="Type your password"
-                                            required
-                                        />
-                                    </Form.Group>
-
-                                    <Button
-                                        id="user-button"
-                                    >
-                                        LOGIN
-                                    </Button>
-                                    <hr></hr>
-                                    <Button
-                                        id="visitor-button"
-                                    >
-                                        CONTINUE AS A VISITOR
-                                    </Button>
-                                    <Button
-                                        id="create-account-button"
-                                    >
-                                        CREATE ACCOUNT
-                                    </Button>
-                                </Form>
-                            </Col>
-                        </Row>
-                    </div>
-                </BrowserRouter>
+            <div className="chapters-page">
+              <BrowserRouter forceRefresh={true}>
+                <div className="chapters-box">
+                  <Row>
+                    <Col md={6}>
+                      <Image id="gsggb-logo" alt="logo" src={gsggb_logo} />
+                    </Col>
+                    <Col md={6}>
+                      <div>
+                        <h1 className="chapters-title">GSGGB Chapters</h1>
+                        <br/>
+                        <Button className="chapters-button" id="uoft-button">
+                          <Image id="uoft-logo" alt="logo" src={uoft_logo} />
+                          University of Toronto
+                        </Button>
+                      </div>
+                      <br/>
+                      <div>
+                        <Button className="chapters-button" id="mcmaster-button">
+                          <Image id="mcmaster-logo" alt="logo" src={mcmaster_logo} />
+                          McMaster University
+                        </Button>
+                      </div>
+                      <br/>
+                      <div>
+                        <Button className="chapters-button" id="queens-button">
+                          <Image id="mcmaster-logo" alt="logo" src={queens_logo} />
+                          Queen's University
+                        </Button>
+                      </div>
+                    </Col>
+                  </Row>
+                </div>
+              </BrowserRouter>
             </div>
         );
     }
