3b178ff8c4673fcf19fdce6362b136ed90334848
Jedid Ahn <jedid.ahn@mail.utoronto.ca>
2021-05-24 23:13:47 -0400

    Added photo banner to sponsors page and added responsiveness.

--- client/src/react-components/Sponsors/index.js
+++ client/src/react-components/Sponsors/index.js
@@ -4,6 +4,7 @@ import { Image, Row, Col } from "react-bootstrap";
 
 import "./styles.css";
 import "./styles-mobile.css";
+import sponsorsPrincetonReview from "./static/sponsors-princeton-review.jpg";
 import { PLATINUM, GOLD, SILVER, BRONZE, PARTNERS, SPECIAL } from './sponsors.js';
 
 class Sponsors extends Component {
@@ -30,69 +31,72 @@ class Sponsors extends Component {
     render() {
         return(
             <BrowserRouter forceRefresh={true}>
-              <br /><br />
-              <div className="sponsors-title-section">
-                  <h2 className="sponsors-page-title">OUR SPONSORS</h2>
-                  <br />
-                  <h4 className="sponsors-page-subtitle">Thank-you to our sponsors for supporting GSGGB and its chapters!</h4>
-              </div>
+                <div className="sponsors-photo-container">
+                    <Image className="sponsors-photo" alt="2019-2020 Introduction to R Workshop" src={sponsorsPrincetonReview} />
+                    <span className="sponsors-photo-title">
+                        Our Sponsors
+                        <h3 className="sponsors-photo-subtitle">
+                            Thank-you to our sponsors for supporting GSGGB and its chapters!
+                        </h3>
+                    </span>
+                </div>
 
-              <br /><br /><br />
-              <h4 className="sponsors-header" id="platinum-sponsor">PLATINUM: Awarded to sponsors with a contribution of $1000 or more.</h4>
-              <br />
-              <div className="container">
-                  <Row>
-                      {PLATINUM.map(this.renderSponsors)}
-                  </Row>
-              </div>
-              <br />
+                <br /><br /><br /><br />
+                <h4 className="sponsors-header" id="platinum-sponsor">PLATINUM: Awarded to sponsors with a contribution of $1000 or more.</h4>
+                <br />
+                <div className="container">
+                    <Row>
+                        {PLATINUM.map(this.renderSponsors)}
+                    </Row>
+                </div>
+                <br />
 
-              <br /><br />
-              <h4 className="sponsors-header" id="gold-sponsor">GOLD: Awarded to sponsors with a contribution of $500 or more.</h4>
-              <br />
-              <div className="container">
-                  <Row>
-                      {GOLD.map(this.renderSponsors)}
-                  </Row>
-              </div>
-              <br/>
+                <br /><br />
+                <h4 className="sponsors-header" id="gold-sponsor">GOLD: Awarded to sponsors with a contribution of $500 or more.</h4>
+                <br />
+                <div className="container">
+                    <Row>
+                        {GOLD.map(this.renderSponsors)}
+                    </Row>
+                </div>
+                <br/>
 
-              <br /><br />
-              <h4 className="sponsors-header" id="silver-sponsor">SILVER: Awarded to sponsors with a contribution of $250 or more.</h4>
-              <br />
-              <div className="container">
-                  <Row>
-                      {SILVER.map(this.renderSponsors)}
-                  </Row>
-              </div>
-              <br/>
+                <br /><br />
+                <h4 className="sponsors-header" id="silver-sponsor">SILVER: Awarded to sponsors with a contribution of $250 or more.</h4>
+                <br />
+                <div className="container">
+                    <Row>
+                        {SILVER.map(this.renderSponsors)}
+                    </Row>
+                </div>
+                <br/>
 
-              <br /><br />
-              <h4 className="sponsors-header" id="bronze-sponsor">BRONZE: Awarded to sponsors with a contribution of $100 or more.</h4>
-              <br />
-              <div className="container">
-                  <Row>
-                      {BRONZE.map(this.renderSponsors)}
-                  </Row>
-              </div>
+                <br /><br />
+                <h4 className="sponsors-header" id="bronze-sponsor">BRONZE: Awarded to sponsors with a contribution of $100 or more.</h4>
+                <br />
+                <div className="container">
+                    <Row>
+                        {BRONZE.map(this.renderSponsors)}
+                    </Row>
+                </div>
 
-              <br /><br />
-              <h4 className="sponsors-header" id="partner-sponsor">Thanks to our U of T club partners for their support!</h4>
-              <br />
-              <div className="container">
-                  <Row>
-                      {PARTNERS.map(this.renderSponsors)}
-                  </Row>
-              </div>
+                <br /><br />
+                <h4 className="sponsors-header" id="partner-sponsor">Thanks to our U of T club partners for their support!</h4>
+                <br />
+                <div className="container">
+                    <Row>
+                        {PARTNERS.map(this.renderSponsors)}
+                    </Row>
+                </div>
 
-              <br /><br />
-              <h4 className="sponsors-header" id="special-sponsor">A special thank-you to the Rare Disease Foundation for our partnership!</h4>
-              <br />
-              <div className="container">
-                  <Row>
-                      {SPECIAL.map(this.renderSponsors)}
-                  </Row>
-              </div>
+                <br /><br />
+                <h4 className="sponsors-header" id="special-sponsor">A special thank-you to the Rare Disease Foundation for our partnership!</h4>
+                <br />
+                <div className="container">
+                    <Row>
+                        {SPECIAL.map(this.renderSponsors)}
+                    </Row>
+                </div>
             </BrowserRouter>
         )
     }
